69be7008029b1e0928da56e4a3618cc24034603a
Raj Kiran <rajkirangs25@gmail.com>
2026-06-08 22:54:34 +0530

    Project Done

---
+++ AirBNB End-To-End/airbnb_aws_project/models/gold/obt.sql
@@ -0,0 +1,35 @@
+{% set configs = [
+    {
+        "table": "AIRBNB.SILVER.SILVER_BOOKINGS",
+        "columns": "silver_bookings.*",
+        "alias": "silver_bookings"
+    },
+    {
+        "table": "AIRBNB.SILVER.SILVER_LISTINGS",
+        "columns": "silver_listings.HOST_ID, silver_listings.PROPERTY_TYPE, silver_listings.ROOM_TYPE, silver_listings.CITY, silver_listings.COUNTRY, silver_listings.ACCOMMODATES, silver_listings.BEDROOMS, silver_listings.BATHROOMS, silver_listings.PRICE_PER_NIGHT, silver_listings.PRICE_PER_NIGHT_TAG, silver_listings.CREATED_AT AS LISTINGS_CREATED_AT",
+        "alias": "silver_listings",
+        "join_condition": "silver_bookings.listing_id = silver_listings.listing_id"
+    },
+    {
+        "table": "AIRBNB.SILVER.SILVER_HOSTS",
+        "columns": "silver_hosts.HOST_NAME, silver_hosts.HOST_SINCE, silver_hosts.IS_SUPERHOST, silver_hosts.RESPONSE_RATE, silver_hosts.RESPONSE_RATE_QUALITY, silver_hosts.CREATED_AT AS HOST_CREATED_AT",
+        "alias": "silver_hosts",
+        "join_condition": "silver_listings.host_id = silver_hosts.host_id"
+    }
+]%}
+
+
+
+SELECT
+    {% for config in configs %}
+        {{ config['columns'] }} {% if not loop.last %}, {% endif %}
+    {% endfor %}
+FROM
+    {% for config in configs %}
+        {% if loop.first %}
+            {{ config['table'] }} AS {{ config['alias'] }}
+        {% else %}
+        LEFT JOIN {{ config['table'] }} AS {{ config['alias'] }}
+        ON {{ config['join_condition'] }}
+        {% endif %}
+    {% endfor %}
